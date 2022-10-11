79110ae8b912cfc17b6eca056e7c0bb9d31a55a4
Farah <farahizzatimdrashid@gmail.com>
2022-10-11 15:55:22 +0800

    28

--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -3,7 +3,7 @@ const LandingPage: React.FC = () => {
     <div>
       <div className="mt-5 mb-5">
         <p className="text-center text-5xl">Hello, </p>
-        <p className="text-center text-5xl">Farah 2700</p>
+        <p className="text-center text-5xl">Farah 28</p>
       </div>
 
       <div className="mt-5 mb-5 flex justify-center">
